20857949faa87a0ab2d60d7ab7259b0348373f70
Andy Park <apark1214@gmail.com>
2021-02-19 11:12:35 -0800

    Accordion in redux hooks

---
+++ widgets/src/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+/* Define a functional component now with redux and hooks */
+
+export default () => {
+  return <h1>Widgets App</h1>;
+};
---
+++ widgets/src/components/accordion.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+
+export default function Accordion () {
+  return <h1>Widgets App</h1>;
+};
