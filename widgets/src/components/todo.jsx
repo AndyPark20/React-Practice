436bfa0e0fa4e75527c4aa2650b6f99d1b26db15
Andy Park <apark1214@gmail.com>
2021-03-02 15:45:23 -0800

    added onchange event on text input

--- widgets/src/components/todo.jsx
+++ widgets/src/components/todo.jsx
@@ -1,10 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useState,useEffect } from 'react';
 import { FormControl } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import InputGroup, { prepend, Text } from 'react-bootstrap/InputGroup';
 
 const Todo = () => {
-  // const [name,setName] = useEffect
+  const [name,setName] = useState('')
+
+// useEffect(()=>{
+
+//   console.log(name)
+
+// },[name])
+
+
 
   return (
     <div className="container">
@@ -14,11 +22,11 @@ const Todo = () => {
             <InputGroup.Prepend>
               <InputGroup.Text id="inputGroup-sizing-default">Names</InputGroup.Text>
             </InputGroup.Prepend>
-            <FormControl aria-label="Default" aria-describedby="inputGroup-sizing-default" />
+            <FormControl onChange={(e)=>setName(e.target.value)} aria-label="Default" aria-describedby="inputGroup-sizing-default" />
           </InputGroup>
         </div>
         <div className="col-12">
-          <Button className="float-right w-25" variant="primary" type="submit">Submit</Button>
+          <Button onClick={()=>{console.log(name)}} className="float-right w-25" variant="primary" type="submit">Submit</Button>
         </div>
       </div>
     </div>
