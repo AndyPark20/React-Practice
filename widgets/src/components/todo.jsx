aa569a3908d229864effa21f1c30f3dbc34b0c6c
Andy Park <apark1214@gmail.com>
2021-03-02 15:37:39 -0800

    added submit button and added onChange event handler

--- widgets/src/components/todo.jsx
+++ widgets/src/components/todo.jsx
@@ -1,21 +1,25 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { FormControl } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import InputGroup, { prepend, Text } from 'react-bootstrap/InputGroup';
 
 const Todo = () => {
-// const [name,setName] = useEffect
+  // const [name,setName] = useEffect
 
   return (
     <div className="container">
       <div className="row w-100">
-        <InputGroup className="mb-3">
-          <InputGroup.Prepend>
-            <InputGroup.Text id="inputGroup-sizing-default">Names</InputGroup.Text>
-          </InputGroup.Prepend>
-          <FormControl  aria-label="Default" aria-describedby="inputGroup-sizing-default"/>
-        </InputGroup>
-        <Button className="w-25" variant= "primary" type="submit">Submit</Button>
+        <div className="col-12">
+          <InputGroup className="mb-3">
+            <InputGroup.Prepend>
+              <InputGroup.Text id="inputGroup-sizing-default">Names</InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl aria-label="Default" aria-describedby="inputGroup-sizing-default" />
+          </InputGroup>
+        </div>
+        <div className="col-12">
+          <Button className="float-right w-25" variant="primary" type="submit">Submit</Button>
+        </div>
       </div>
     </div>
   )
