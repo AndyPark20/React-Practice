b074326d10dedbfaff6c2469774e549f96c76225
Andy Park <apark1214@gmail.com>
2021-03-02 20:21:02 -0800

    completed random generator

--- widgets/src/components/panel.jsx
+++ widgets/src/components/panel.jsx
@@ -1,39 +1,52 @@
-import React, {useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 const Panel = ({ value }) => {
-  const [random,getRandom] = useState('');
-  useEffect(()=>{
-      value.sort(()=>Math.random()-0.5)
-      getRandom(value);
-  },[value])
+  const [random, getRandom] = useState([]);
+
+
+  useEffect(() => {
+    const steady = value.slice()
+    steady.sort(() => Math.random() - 0.5)
+    getRandom(steady);
+  }, [value])
 
 
   const renderIt = () => {
     const circle = value.map((name, index) => {
       return (
-        <div className="container text-center" key={index}>
-          <div className="row">
-            <div className="col-12 text-center">
-              <h3 className="nothing">{name}</h3>
-            </div>
-          </div>
+        <div key={index}>
+          <h3 className="nothing">{name}</h3>
         </div>
       )
     })
     return circle;
   }
 
+  const generate =()=>{
+    const rando = random.map((values,index)=>{
+      return(
+        <div>
+          <h3 className="nothing">{values}</h3>
+        </div>
+      )
+    })
+    return rando;
+  }
 
 
   return (
     <div className="conainter">
-      <div className="row d-flex">
+      <div className="row w-100 d-flex flex-row">
         <div className="col-6">
+          <h1>Original</h1>
           {renderIt()}
         </div>
         <div className="col-6">
-          {random}
+          <h1>Random</h1>
+          <div className="d-flex flex-column ">
+            {generate()}
+          </div>
         </div>
       </div>
     </div>
